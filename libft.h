9bc4babe3bb9c5e05a7acec2238ce3da458ff3f9
Colin Projean <cprojean@z2r5p7.42lyon.fr>
2022-11-09 16:29:46 +0100

    lib

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cprojean <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/08 16:03:39 by cprojean          #+#    #+#             */
+/*   Updated: 2022/11/09 14:04:08 by cprojean         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//ft_strlen
+int	ft_strlen(const char *s);
+//ft_memset
+void	*ft_memset(void *b, int c, size_t len);
+
+#endif
