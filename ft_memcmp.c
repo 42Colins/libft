7f4d2d6283e630ab92716d456575a912f048cdf5
Colin Projean <cprojean@z1r7p3.42lyon.fr>
2022-11-13 14:58:36 +0100

    enfincamake

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: cprojean <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/11 13:12:20 by cprojean          #+#    #+#             */
-/*   Updated: 2022/11/11 13:21:47 by cprojean         ###   ########.fr       */
+/*   Updated: 2022/11/13 14:57:16 by cprojean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,15 @@
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
 	int	i;
+	char	*array;
+	char	*string;
 
+	array = (char *)s1;
+	string = (char *)s2;
 	i = 0;
 	while ((size_t) i <= n)
 	{
-		if (s1[i] != s2[i])
+		if (array[i] != string[i])
 			return (s1 - s2);
 		i++;
 	}
