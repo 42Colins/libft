2bd2c2f5f7e1b70f0b689af19fb2999d901e89b5
Colin Projean <cprojean@z1r7p3.42lyon.fr>
2022-11-13 14:59:53 +0100

    stopetreidiot

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: cprojean <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/12 16:15:30 by cprojean          #+#    #+#             */
-/*   Updated: 2022/11/13 12:56:24 by cprojean         ###   ########.fr       */
+/*   Updated: 2022/11/13 14:59:19 by cprojean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ char    *ft_strdup(const char *s1)
     runner = 0;
     size = ft_strlen(s1);
     dup = malloc(sizeof(char) * size + 1);
-	if (dup = 0)
+	if (dup == 0)
 		return (0);
     while (runner <= size)
     {
