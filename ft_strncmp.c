409a4c2504683da57d0b79a4363d94ad3496e2ee
Colin Projean <cprojean@z2r5p5.42lyon.fr>
2022-11-14 17:45:39 +0100

    strncmp marche

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: cprojean <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/08 13:17:39 by cprojean          #+#    #+#             */
-/*   Updated: 2022/11/14 16:36:21 by cprojean         ###   ########.fr       */
+/*   Updated: 2022/11/14 17:44:48 by cprojean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	int		index;
+	size_t			index;
 	unsigned char	*array;
 	unsigned char	*string;
 
@@ -23,25 +23,22 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	index = 0;
 	if (n == 0)
 		return (0);
-	while ((size_t)index <= n)
+	while (((array[index]) || (string[index])) && ((size_t) index != n))
 	{
-
-		if (s1[index] != s2[index])
-			return (s1[index] - s2[index]);
-		/*else if (s1[index] > s2[index])
-			return (s1[index] - s2[index]);*/
-		else
-			index++;
+		if (array[index] != string[index])
+			return ((array[index] + '0') - (string[index] + '0'));
+		index++;
 	}
 	return (0);
 }
-
+/*
 int main()
 {
-	const char *s1 = "test\0";
-	const char *s2 = "test\200";
-	size_t n = 6;
+	const char *s1 = "abcdefgh";
+	const char *s2 = "abcdwxyz";
+	size_t n = 4;
 
 	printf("%d\n", ft_strncmp(s1, s2, n));
-	printf("%d", strncmp(s1,s2,n));
+	printf("%d\n", strncmp(s1,s2,n));
 }
+*/
