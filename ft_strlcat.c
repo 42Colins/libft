04447e8daa8a5fb3708f222955b1f220efb5cfd8
Colin Projean <cprojean@z2r4p8.42lyon.fr>
2022-11-16 11:23:41 +0100

    strlcat still needs fix but let's moove on

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -1,48 +1,67 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*   ft_strlcat.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cprojean <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/08 13:16:00 by cprojean          #+#    #+#             */
-/*   Updated: 2022/11/14 17:46:48 by cprojean         ###   ########.fr       */
+/*   Created: 2022/11/15 18:07:13 by cprojean          #+#    #+#             */
+/*   Updated: 2022/11/15 18:14:28 by cprojean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+/*size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	int	index;
 	int	start;
+	int	returnval;
 
+	index = 0;
+	returnval = ft_strlen(dst) + ft_strlen(src);
 	start = ft_strlen(dst);
+	if ((dstsize == 0) || ((size_t)start >= dstsize))
+		return (returnval);
+	while ((size_t)start + index < dstsize)
+	{
+		dst[start + index] = src[index];
+		index++;
+	}
+	dst[start + index + 1] = '\0';
+	return (returnval);
+}*/
+
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+{
+	int		index;
+	size_t	start;
+
 	index = 0;
 	if (dstsize == 0)
 		return (ft_strlen(src));
-	if ((size_t)ft_strlen(dst) - 1 + (size_t)ft_strlen(src) < dstsize)
-		return (index);
-	while ((size_t)index < dstsize)
+	start = ft_strlen(dst);
+	if (start >= dstsize)
+		return (start + ft_strlen(src));
+	while ((start + index < dstsize - 1) && (src[index] != '\0'))
 	{
-		dst[start] = src[index];
+		dst[start + index] = src[index];
 		index++;
-		start++;
 	}
-	dst[index] = '\0';
-	return (index);
+	dst[index + start] = '\0';
+	return (start + ft_strlen(src));
 }
 /*
 int main()
 {
-	char 	*dst = "a";
-	char	*dest= "a";
-	const char *str = "cava";
-	size_t	dstsize = 5;
+	char 	dst[100] = "a";
+	char	dest[100]= "a";
+	const char *str = "lorem ipsum dolor sit amet";
+	size_t	dstsize = 99;
 
 	printf("%zu\n",ft_strlcat(dst, str, dstsize));
 	printf("%s\n", dst);
 	printf("%zu\n", strlcat(dest, str, dstsize));
 	printf("%s\n", dest);
-}
-*/
+}*/
+
