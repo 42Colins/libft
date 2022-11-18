7eae37fbd50e57f3547c0a610c562119a5b8a746
Colin Projean <cprojean@z2r3p4.42lyon.fr>
2022-11-18 17:23:26 +0100

    striteri je suis stupide

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,23 +6,23 @@
 /*   By: cprojean <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/18 15:50:19 by cprojean          #+#    #+#             */
-/*   Updated: 2022/11/18 16:41:40 by cprojean         ###   ########.fr       */
+/*   Updated: 2022/11/18 17:22:39 by cprojean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// void	ft_striteri(char *s, void (*f)(unsigned int, char*))
-// {
-// 	int	index;
+void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+{
+	int	index;
 
-// 	index = 0;
-// 	while(s[index])
-// 	{
-// 		s[index] = (*f)(index, &s[index]);
-// 		index++;
-// 	}
-// }
+	index = 0;
+	while (s[index])
+	{
+		f(index, &s[index]);
+		index++;
+	}
+}
 
 // void	fonction(unsigned int i, char*)
 // {
