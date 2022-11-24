07bd634420a626a2be96ac6bcc53b2959d4de760
Colin Projean <colinprojean@MacBook-Air-de-Colin.local>
2022-11-24 14:39:23 +0100

    ijustwanttopull

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: cprojean <cprojean@42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/13 12:53:52 by cprojean          #+#    #+#             */
-/*   Updated: 2022/11/23 11:01:15 by cprojean         ###   ########.fr       */
+/*   Updated: 2022/11/24 14:34:18 by cprojean         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t count, size_t size)
 	char	*calloc;
 	size_t	total;
 
-	total = size * count;git 
+	total = size * count;
 	if (((total) / SIZE_MAX) > 1)
 		return (0);
 	else if (total == 0)
